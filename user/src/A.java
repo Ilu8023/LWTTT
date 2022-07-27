80009f2c4b54749be62f6f68a76cff592f6eac4c
Your Name <you@example.com>
2022-07-27 14:19:14 +0800

    '12'

--- user/src/A.java
+++ user/src/A.java
@@ -1,2 +1,37 @@
 public class A {
-}
+
+        /**
+         * 学号
+         * 姓名
+         * 年龄
+         * 性别
+         */
+        public String studentNu;
+        public String studentName;
+
+
+        //行为
+        /**
+         * 上晚自习
+         * 泡图书馆
+         * 上课
+         * 做广播体操
+         * f（x）=ax+b
+         */
+        public void goToNightClasses(){
+            System.out.println("我们在上晚自习");
+        }
+
+        public void BubbleLibrary(){
+            System.out.println("我们在泡图书馆");
+        }
+
+        public void attendClass(){
+            System.out.println("我们在上晚自习");
+        }
+
+        public void BroadcastGymnastics(){
+            System.out.println("我们在做广播体操");
+        }
+    }
+
